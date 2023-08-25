5f9ac7d06f3462e452e72a8a8dbeee833df35295
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2023-08-25 18:54:34 +0530

    GrooveProjectDone

---
+++ app.js
@@ -0,0 +1,12 @@
+var placeorder = document.getElementById("placeorder")
+console.log(placeorder)
+var Thanks = document.getElementById("Thanks")
+console.log(Thanks)
+
+
+placeorder.onclick=()=>{
+   placeorder.style.display="none"
+   
+   Thanks.style.display="block"
+}
+
